4839905ace5d96d4c64c5b6447bb98fd1d22be56
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-08-30 18:08:44 +0530

    Tut28 - Using Fetch API in React to populate NewsItems (Paginations)

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -8,17 +8,48 @@ export class News extends Component {
     console.log("Hello i am a constructor from  News Component");
     this.state = {
         articles: [],
-        loading: false
+        loading: false,
+        page:1
     };
   }
 
   async componentDidMount() {
-      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=273c05648e1946a8b3fbbe667405a88d";
+      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=273c05648e1946a8b3fbbe667405a88d&page=1$pageSize=20";
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(parsedData)
-      this.setState({ articles: parsedData.articles});
+      this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
 
+  }
+
+  handlePrevClick = async () => {
+        console.log("Previous Page");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=273c05648e1946a8b3fbbe667405a88d&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData)
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        });
+
+  }
+
+  handleNextClick = async () => {
+        console.log("Next Page");
+        if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+            alert("Page Has Ended!");
+        }
+        else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=273c05648e1946a8b3fbbe667405a88d&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData)
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            });
+        }
   }
   
 
@@ -26,14 +57,18 @@ export class News extends Component {
     return (
       <>
         <div className="container my-3">
-          <h1>NewsMonkey - Top Headlines</h1>
-          <div className="row">
-              {this.state.articles.map((element)=>{
-                    return <div className="col-md-4" key={element.url}>
-                        <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url} />
-                    </div>
-              })}
-          </div>
+            <h1>NewsMonkey - Top Headlines</h1>
+            <div className="row">
+                {this.state.articles.map((element)=>{
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
+                })}
+            </div>
+            <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page<=1} type="button" className="btn btn-primary" onClick={this.handlePrevClick}>&#8612; Previous</button>
+            <button type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &#8614;</button>
+            </div>
         </div>
       </>
     );
