b63eb77f0aadf8c2601651a7782bb1c136a638ff
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-08-30 16:56:11 +0530

    Tut27 - Using Fetch API in React to populate NewsItems

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -5,8 +5,8 @@ export class NewsItem extends Component {
         let {title, description, imageUrl, newsUrl} = this.props;
         return (
             <>
-                <div className="card my-2" style={{width: '18rem'}}>
-                    <img src={imageUrl} className="card-img-top" alt="..." style={{width: '286px', height: '160px'}}/>
+                <div className="card my-4" style={{width: '18rem'}}>
+                    <img src={!imageUrl?"https://fdn.gsmarena.com/imgroot/news/21/08/iphone-13-leo-satellite-connectivity-rumor/-476x249w4/gsmarena_00.jpg":imageUrl} className="card-img-top" alt="..." style={{width: '286px', height: '160px'}}/>
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
