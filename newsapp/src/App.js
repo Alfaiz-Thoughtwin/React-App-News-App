b2c11b7d1a9d3a6000d053c9787748ef6d50707e
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-08-31 12:57:43 +0530

    Tut29 - Adding loading spinner & variable pageSize to NewsMonkey

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+// import Loader from "react-loader-spinner";
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
@@ -8,8 +10,15 @@ export default class App extends Component {
   render() {
     return (
       <>
+        {/* <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+        /> */}
         <NavBar/>
-        <News/>
+        <News pageSize={5}/>
       </>
     )
   }
--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
 
@@ -14,60 +15,68 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=273c05648e1946a8b3fbbe667405a88d&page=1$pageSize=20";
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=273c05648e1946a8b3fbbe667405a88d&page=1$pageSize=${this.props.pageSize}`;
+      this.setState({loading: true});
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(parsedData)
-      this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
-
+      this.setState({
+        articles: parsedData.articles,
+        totalResults: parsedData.totalResults,
+        loading: false
+      });
   }
 
   handlePrevClick = async () => {
         console.log("Previous Page");
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=273c05648e1946a8b3fbbe667405a88d&page=${this.state.page - 1}&pageSize=20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=273c05648e1946a8b3fbbe667405a88d&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({loading: true});
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData)
         this.setState({
             page: this.state.page - 1,
-            articles: parsedData.articles
+            articles: parsedData.articles,
+            loading: false
         });
-
   }
 
   handleNextClick = async () => {
         console.log("Next Page");
-        if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+        if(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
             alert("Page Has Ended!");
         }
         else {
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=273c05648e1946a8b3fbbe667405a88d&page=${this.state.page + 1}&pageSize=20`;
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=273c05648e1946a8b3fbbe667405a88d&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            this.setState({loading: true});
             let data = await fetch(url);
             let parsedData = await data.json();
             console.log(parsedData)
             this.setState({
                 page: this.state.page + 1,
-                articles: parsedData.articles
+                articles: parsedData.articles,
+                loading: false
             });
         }
   }
   
-
   render() {
     return (
       <>
         <div className="container my-3">
-            <h1>NewsMonkey - Top Headlines</h1>
+            <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+            <hr className="my-4" />
+            {this.state.loading && <Spinner/>}
             <div className="row">
-                {this.state.articles.map((element)=>{
-                        return <div className="col-md-4" key={element.url}>
+                {!this.state.loading && this.state.articles.map((element)=>{
+                        return <div className="col-md-4 col-sm-6" key={element.url}>
                             <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                 })}
             </div>
             <div className="container d-flex justify-content-between">
-            <button disabled={this.state.page<=1} type="button" className="btn btn-primary" onClick={this.handlePrevClick}>&#8612; Previous</button>
-            <button type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &#8614;</button>
+            <button disabled={this.state.page<=1} type="button" className="btn btn-primary" onClick={this.handlePrevClick}>&laquo; Previous</button>
+            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &raquo;</button>
             </div>
         </div>
       </>
