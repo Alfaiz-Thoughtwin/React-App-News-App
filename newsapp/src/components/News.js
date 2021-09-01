f69fdd14452c9d07f0c786da68eefd1a839acf51
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-09-01 13:49:01 +0530

    Tut34 - React Component Lifecycle & Lifecycle methods

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -17,14 +17,19 @@ export class News extends Component {
     category : PropTypes.string
   }
 
-  constructor() {
-    super();
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props) {
+    super(props);
     console.log("Hello i am a constructor from  News Component");
     this.state = {
         articles: [],
         loading: false,
         page:1
     };
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
   
 
@@ -78,7 +83,7 @@ export class News extends Component {
     return (
       <>
         <div className="container my-3">
-            <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+            <h1 className="text-center">NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
             <hr className="my-4" />
             {this.state.loading && <Spinner/>}
             <div className="row">
