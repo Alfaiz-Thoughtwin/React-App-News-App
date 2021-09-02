321633dc72ee686d58db6be47838694c360a99fb
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-09-02 13:26:23 +0530

    Tut39 - Changing Class based NewsMonkey components to Functional based (except:App.js)

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-
-// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-// import Loader from "react-loader-spinner";
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
@@ -28,13 +25,6 @@ export default class App extends Component {
     return (
       <>
         <Router>
-          {/* <Loader
-          type="Puff"
-          color="#00BFFF"
-          height={100}
-          width={100}
-          timeout={3000} //3 secs
-          /> */}
           <NavBar/>
           <LoadingBar
             height={3}
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,10 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import moment from 'moment';
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
+const NewsItem = (props) => {
+  
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
     return (
       <>
         <div className="card my-4" style={{ width: "18rem" }}>
@@ -46,7 +45,7 @@ export class NewsItem extends Component {
         </div>
       </>
     );
-  }
+  
 }
 
 export default NewsItem;
